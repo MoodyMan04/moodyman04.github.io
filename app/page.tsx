b1c467c1ba1c9447ca3391220944b2a3ca180af5
Gunnar Moody <moodygman04@gmail.com>
2024-06-08 18:59:42 -0600

    About and Style
    - added more description to the About/Who Am I? section
    - adjusted the style of the crt and scanline filter to follow the screen as you scroll
    - added a style (small-br) for the <br> tag to reduce the space created by it.

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,41 @@ export default function Home() {
         <div className="grid grid-cols-1 mt-4 md:grid-cols-2 gap-8">
           <div className="p-4 bg-blue-400 bg-opacity-25 rounded-md">
             <h1 className="chromatic">Who Am I?</h1>
-            <h2 className="chromatic">Gunnar Moody | Software Engineer</h2>
+            <h2 className="chromatic">
+              Gunnar Moody | Full-Stack Software Developer
+            </h2>
             <p className="chromatic">
+              <b>
+                <br className="small-br" />
+                Skills:
+                <br className="small-br" />
+                Java | Python | C# | SQL | HTML | CSS | JavaScript and
+                TypeScript
+                <br className="small-br" />
+                Git | GitHub | MySQL | PostgreSQL | React.js | Next.js | Django
+                | ASP.NET
+              </b>
+              <br className="small-br" />
+              <br className="small-br" />
               Hello! My name is Gunnar Moody! I am a dedicated software engineer
-              and current student of NDSU&#39;s class of 2025! I am pursuing a
-              bachelor&#39;s degree in Computer Science with an interest in
-              Cybersecurity and a minor in A.I.
+              and current student of NDSU&#39;s class of 2025, where I am
+              pursuing a bachelor&#39;s degree in Computer Science with an
+              interest in Cybersecurity and a minor in A.I.
+              <br className="small-br" />
+              <br className="small-br" />
+              While at NDSU, I have gained experience through many different
+              projects, including school team projects and working as a game
+              developer on our ACM&#39;s Byte-le Royale 2024-25!
+              <br className="small-br" />
+              <br className="small-br" />I am currently working for Fast
+              Enterprises over the summer as a Implementation Intern with the
+              Colorado Tax team!
+              <br className="small-br" />
+              <br className="small-br" />I love to learn and find everything
+              computers fascinating! When I am not studying or working, I am
+              typically working on personal projects, such as Unity games, or
+              enjoying one of many hobbies, such as watching movies, reading a
+              book, practicing guitar, etc.
             </p>
           </div>
           <div className="p-4 bg-blue-400 bg-opacity-25 rounded-md">
