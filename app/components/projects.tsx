67487109eff2448022c4f2a72e1ed6f608a459f3
Gunnar Moody <moodygman04@gmail.com>
2024-07-01 15:03:26 -0600

    Update projects.tsx
    - added some extra repos (byte_engine, Byte-le-2024)
    - added more comments
    - added logic to add more repos in the future easier
    - added new section for other repos (ndsu acm byte-le repos)

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -10,15 +10,31 @@ Thank you.
 */
 
 export default function Projects() {
-  // Logic to call GitHub API and fetch owned and public repos
+  // Logic to call GitHub API and fetch owned and public repos, as well as some Byte-le repos
+  // Set states
   const [repos, setRepo] = useState<any[]>([]);
+  const [reposOther, setOtherRepo] = useState<any[]>([]);
 
+  // Get repos
   const fetchRepos = async () => {
-    const response = await fetch(
-      "https://api.github.com/users/moodyMan04/repos"
-    );
-    const repos = await response.json();
+    const response = {
+      "My Repos": await fetch("https://api.github.com/users/moodyMan04/repos"),
+      "Byte Engine": await fetch(
+        "https://api.github.com/repos/acm-ndsu/byte_engine"
+      ),
+      "Byte 2024": await fetch(
+        "https://api.github.com/repos/acm-ndsu/Byte-le-2024"
+      ),
+    };
+
+    // Get jsons from repos
+    const repos = await response["My Repos"].json();
+    const reposByteEngine = await response["Byte Engine"].json();
+    const reposByte2024 = await response["Byte 2024"].json();
+
+    // Call set repo methods on repo jsons
     setRepo(repos);
+    setOtherRepo([reposByteEngine, reposByte2024]);
   };
   useEffect(() => {
     fetchRepos();
@@ -28,38 +44,67 @@ export default function Projects() {
   return (
     <div>
       <h1 className="chromatic">Projects!</h1>
-      <h3 className="chromatic">GitHub Repos:</h3>
+      <h2 className="chromatic">GitHub Repos:</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-1">
         {repos.map((user) => {
           const { id, name, html_url, language, owner, description } = user;
-          return (
-            <div
-              key={id}
-              className="m-2 bg-black bg-opacity-50 rounded-lg hover:bg-opacity-75"
-            >
-              <Link
-                href={html_url}
-                target="_blank"
-                className="p-4 block w-full h-full"
-              >
-                <div className="flex items-center">
-                  <img
-                    src={`${owner.avatar_url}`}
-                    alt="Space Dog"
-                    width={75}
-                    className="rounded-full"
-                  />
-                  <h2 className="ml-4 chromatic">{name}</h2>
-                </div>
-                <br className="small-br" />
-                <h3 className="chromatic">Language: {language}</h3>
-                <br className="small-br" />
-                <p className="chromatic">{description}</p>
-              </Link>
-            </div>
+          return loadProjectCards(
+            id,
+            name,
+            html_url,
+            language,
+            owner,
+            description
           );
         })}
       </div>
+      <h2 className="chromatic">NDSU ACM Byte-le Repos:</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-1">
+        {reposOther.map((user) => {
+          const { id, name, html_url, language, owner, description } = user;
+          return loadProjectCards(
+            id,
+            name,
+            html_url,
+            language,
+            owner,
+            description
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+// Function for displaying projects as cards on the website
+function loadProjectCards(
+  id: number,
+  name: string,
+  html_url: string,
+  language: string,
+  owner: any,
+  description: string
+) {
+  return (
+    <div
+      key={id}
+      className="m-2 bg-black bg-opacity-50 rounded-lg hover:bg-opacity-75"
+    >
+      <Link href={html_url} target="_blank" className="p-4 block w-full h-full">
+        <div className="flex items-center">
+          <img
+            src={`${owner.avatar_url}`}
+            alt="Owner Image"
+            width={75}
+            className="rounded-full"
+          />
+          <h2 className="ml-4 chromatic">{name}</h2>
+        </div>
+        <br className="small-br" />
+        <h3 className="chromatic">Language: {language}</h3>
+        <br className="small-br" />
+        <p className="chromatic">{description}</p>
+      </Link>
     </div>
   );
 }
